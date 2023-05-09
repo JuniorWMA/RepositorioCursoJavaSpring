08fa3d7be3725a9e89dc0b80595dbfe17af4f2fd
Wellisson Junior <junior.wmoreira@gmail.com>
2023-05-09 10:04:33 -0300

    criando o controller e utilizando o postman

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+    
+    @GetMapping
+    public List<User> findAll() {
+       List<User> list = service.findAll(); 
+       return list;
+    }
+    
+}
--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -21,6 +21,10 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    
+    public User() {
+    }
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
