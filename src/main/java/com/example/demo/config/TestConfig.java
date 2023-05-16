a67dd84b42b71fc5b1ff3b3a6c9aa27df3397e2c
Wellisson Junior <junior.wmoreira@gmail.com>
2023-05-16 14:39:36 -0300

    Incluindo o OrderStatus Enum

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.demo.entities.Order;
 import com.example.demo.entities.User;
+import com.example.demo.entities.enums.OrderStatus;
 import com.example.demo.repositories.OrderRepository;
 import com.example.demo.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T10:03:02Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T21:15:57Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID , u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T10:03:02Z"),OrderStatus.WATTING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T21:15:57Z"),OrderStatus.WATTING_PAYMENT, u1);
 
 
 
