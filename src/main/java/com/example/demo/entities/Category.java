b314fd19ee76407de8ec224bffdec0ffff2251dc
Wellisson Junior <junior.wmoreira@gmail.com>
2023-05-22 15:19:53 -0300

    Many to Many da classe Category e Product

--- src/main/java/com/example/demo/entities/Category.java
+++ src/main/java/com/example/demo/entities/Category.java
@@ -3,10 +3,13 @@ package com.example.demo.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -19,7 +22,8 @@ public class Category {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy ="categories" )
     private Set<Product> products = new HashSet<>();
 
     
